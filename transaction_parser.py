80869686beb4d9be0932499093c9f51eb7f7f307
khrisnakamarga <khrisna@uw.edu>
2019-10-19 18:54:20 -0700

    created functions for reading and writing, created a transaction class that stores tipe and jumlah, updates the writer to include each transaction for every date

--- transaction_parser.py
+++ transaction_parser.py
@@ -2,46 +2,65 @@ import pandas as pd
 import numpy as np
 import xlsxwriter
 
-df = pd.read_excel('toBeParsed.xlsx', sheet_name='Kas')
-df = df[np.isfinite(df['NIK'])]  # dropping nonexistent NIKs
-df['Masuk'].fillna(0, inplace=True)  # replace all NaN with 0
-df['Keluar'].fillna(0, inplace=True)  # replace all NaN with 0
+
+class Transaksi:
+    def __init__(self, jumlah, tipe):
+        # TO DO: Privatize
+        self.jumlah = jumlah
+        self.tipe = tipe
+
+    def __repr__(self):
+        return "({} sejumlah Rp{})".format(self.tipe, self.jumlah)
 
 
-# Map<NIK, <Map<Tanggal, List<Map<Type, Value>>>> map;
+def load_file(data_frame):
+    # Map<NIK, <Map<Tanggal, List<Transaksi(jumlah, tipe)>>> map;
 
-nik_dict = dict()  # dictionary that contains all the transaction information
-for index, row, in df.iterrows():
-    curr_nik_dict = None
-    if row['NIK'] not in nik_dict.keys() and row['Tanggal'] not in nik_dict.values():
-        nik_dict[int(row['NIK'])] = {row['Tanggal']: []}
-    elif row['Tanggal'] not in nik_dict[int(row['NIK'])].keys():
-        nik_dict[int(row['NIK'])][row['Tanggal']] = [];
+    nik_dict = dict()  # dictionary that contains all the transaction information
+    for index, row, in data_frame.iterrows():
+        if row['NIK'] not in nik_dict.keys() and row['Tanggal'] not in nik_dict.values():
+            nik_dict[int(row['NIK'])] = {row['Tanggal']: []}
+        elif row['Tanggal'] not in nik_dict[int(row['NIK'])].keys():
+            nik_dict[int(row['NIK'])][row['Tanggal']] = []
 
-    curr_nik_dict = nik_dict[int(row['NIK'])][row['Tanggal']]
-    jumlah = abs(row['Masuk'] + row['Keluar'])  # merge these columns to one
-    curr_nik_dict.append({'Jumlah': jumlah})
-    curr_nik_dict.append({'Transaksi': row['Transaksi']})
+        # Filters recorded transactions to only "Tarikan" or "Tabungan"
+        curr_transaksi = nik_dict[int(row['NIK'])][row['Tanggal']]
+        jumlah = abs(row['Masuk'] + row['Keluar'])  # merge these columns to one
+        curr_transaksi.append(Transaksi(jumlah, row['Transaksi']))
+    print(nik_dict)
+
+    return nik_dict
 
 
-# print(nik_dict)
+def generate_report(nik_dict):
+    workbook = xlsxwriter.Workbook('nik_report.xlsx')
 
-workbook = xlsxwriter.Workbook('nik_report.xlsx')
+    for nik in nik_dict.keys():
+        curr_sheet = workbook.add_worksheet("NIK {}".format(nik))
+        curr_sheet.write(0, 0, "Tanggal")
+        curr_sheet.write(0, 1, "Jumlah Transaksi")
+        curr_sheet.write(0, 2, "Tipe Transaksi")
+        row = 0
+        col = 0
+        for date in nik_dict[nik].keys():
+            item_list = nik_dict[nik][date]
+            # print(item)
+            for item in item_list:
+                row += 1
+                curr_sheet.write(row, col, date)
+                curr_sheet.write(row, col + 1, item.jumlah)
+                curr_sheet.write(row, col + 2, item.tipe)
 
-for nik in nik_dict.keys():
-    curr_sheet = workbook.add_worksheet("NIK {}".format(nik))
-    curr_sheet.write(0, 0, "Tanggal")
-    curr_sheet.write(0, 1, "Jumlah Transaksi")
-    curr_sheet.write(0, 2, "Tipe Transaksi")
-    row = 0
-    col = 0
-    for date in nik_dict[nik].keys():
-        row += 1
-        curr_sheet.write(row, col, date)
-        item = nik_dict[nik][date]
-        print(item)
-        curr_sheet.write(row, col + 1, item[0]["Jumlah"])
-        curr_sheet.write(row, col + 2, item[1]["Transaksi"])
-        row += 1
+    workbook.close()
 
-workbook.close()
+
+if __name__ == "__main__":
+    df = pd.read_excel('toBeParsed.xlsx', sheet_name='Kas')
+    df = df[np.isfinite(df['NIK'])]  # dropping nonexistent NIKs
+    df['Masuk'].fillna(0, inplace=True)  # replace all NaN with 0
+    df['Keluar'].fillna(0, inplace=True)  # replace all NaN with 0
+
+    nik_dict = load_file(df)
+    # print(nik_dict)
+
+    generate_report(nik_dict)
