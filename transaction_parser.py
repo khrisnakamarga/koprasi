d81616ac9f48e0865427e83483096d15444f5f52
khrisnakamarga <khrisna@uw.edu>
2019-10-20 10:26:45 -0700

    cleaned up style

--- transaction_parser.py
+++ transaction_parser.py
@@ -8,6 +8,13 @@ import calendar
 
 
 def monthdelta(d1, d2):
+    """
+    Returns the difference of two datetimes in months
+    :param datetime d1: the earlier date
+    :param datetime d2: the later date
+    :return: the month difference
+    :rtype: int
+    """
     delta = 0
     while True:
         mdays = calendar.monthrange(d1.year, d1.month)[1]
@@ -20,19 +27,36 @@ def monthdelta(d1, d2):
 
 
 class Transaksi:
+    """
+    Transaksi holds the information of a transaction which conveys its amount and its transaction type
+    """
     def __init__(self, jumlah, tipe):
-        # TO DO: Privatize
+        """
+        Initializes the Transaksi object
+        :param float jumlah: the amount of transaction
+        :param str tipe: the type of transaction
+        """
         self.jumlah = jumlah
         self.tipe = tipe
 
     def __repr__(self):
+        """
+        To String method
+        :return: the string representation of Transaksi
+        """
         return "({} sejumlah Rp{})".format(self.tipe, self.jumlah)
 
 
 class EmployeeBalance:
-    INTEREST = 0.012
+    """
+    Keeps track of the employee's balance throughout the date
+    """
+    INTEREST = 0.012  # the interest rate applied to daily average balance at the end of each month
 
     def __init__(self):
+        """
+        Initializes EmployeeBalance
+        """
         self.balance = 0
         self.currDate = None
 
@@ -41,6 +65,14 @@ class EmployeeBalance:
         self.__balanceAccumulation = 0
 
     def update(self, date, transaction, amount):
+        """
+        Updates the current balance given the transaction type, amount, and date
+        :param datetime date: date of the transaction
+        :param str transaction: the type of transaction
+        :param float amount: the amount of transaction
+        :return: the current employee balance
+        :rtype: float
+        """
         if self.currDate is None or self.__startDate is None:
             self.__startDate = pd.to_datetime(date, format="%Y%m%d")
             self.currDate = pd.to_datetime(date, format="%Y%m%d")
@@ -62,6 +94,12 @@ class EmployeeBalance:
         return self.balance
 
     def update_monthly(self, date):
+        """
+        Gets called when there is a change in month
+        :param datetime date: the current transaction's date
+        :return: the monthly added interest
+        :rtype: float
+        """
         end_of_month = calendar.monthrange(self.currDate.year, self.currDate.month)[1]
         end_of_month = datetime(self.currDate.year, self.currDate.month, end_of_month)
         time_range = end_of_month - self.currDate
@@ -80,7 +118,12 @@ class EmployeeBalance:
 
 
 def load_file(data_frame):
-    # Map<NIK, <Map<Tanggal, List<Transaksi(jumlah, tipe)>>> map;
+    """
+    Loads the excel file into cleaned up data structure that groups transactions by NIK and transaction date
+    :param dataFrame data_frame: the data frame containing the excel document
+    :return: the cleaned up data structure version of the excel file
+    :rtype: Map<NIK, <Map<Tanggal, List<Transaksi(jumlah, tipe)>>>
+    """
 
     nik_dict = dict()  # dictionary that contains all the transaction information
     for index, row, in data_frame.iterrows():
@@ -97,6 +140,10 @@ def load_file(data_frame):
 
 
 def generate_report(nik_dict):
+    """
+    Generates the report that also tracks employee balances for each employee
+    :param Map<NIK, <Map<Tanggal, List<Transaksi(jumlah, tipe)>>> nik_dict: the data frame as a data structure
+    """
     workbook = xlsxwriter.Workbook('nik_report.xlsx')
     date_format = workbook.add_format({'num_format': 'dd/mm/yy'})
     money_format = workbook.add_format({'num_format': 'Rp#,##0.00'})
@@ -123,7 +170,8 @@ def generate_report(nik_dict):
                     curr_sheet.write(row, col, date, date_format)
                     curr_sheet.write(row, col + 1, item.jumlah, money_format)
                     curr_sheet.write(row, col + 2, item.tipe)
-                    curr_sheet.write(row, col + 3, employee_balance.update(date, item.tipe, item.jumlah), money_format)
+                    balance = employee_balance.update(date, item.tipe, item.jumlah)
+                    curr_sheet.write(row, col + 3, balance, money_format)
 
     workbook.close()
 
